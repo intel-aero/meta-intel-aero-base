3694ea1f1fb9889ea1bf15832deb04baafd122ee
José Roberto de Souza <jose.souza@intel.com>
2017-08-17 17:03:09 -0700

    initscripts-aero: ds4: Fix enumeration after reboot
    DS4 power was turned on in BIOS 01.00.13 but this is causing DS4 not
    be enumerated after a reboot, so here it is turned off and then on
    fixing the enumeration problem.

--- recipes-core/initscripts-aero/files/ds4.sh
+++ recipes-core/initscripts-aero/files/ds4.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# short-description: Start power DS4 by enabling regulator with GPIO
-# long-descriptioni: The script initializes ds4 (enable power) when 
-# booting to enable driver enumeration once system is ready.
+# DS4 power was turned on in BIOS 01.00.13 but this is causing DS4 not
+# be enumerated after a reboot, so here it is turned off and then on
+# fixing the enumeration problem.
 
 echo 405 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio405/direction
+echo 0 > /sys/class/gpio/gpio405/value
+sleep 1
 echo 1 > /sys/class/gpio/gpio405/value
