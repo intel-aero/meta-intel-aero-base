1e9fdd6ce67a6ef2552a92684650dc29f0e809ad
José Roberto de Souza <jose.souza@intel.com>
2017-10-27 14:37:12 -0700

    recipes-support: scripts: Fix and rewrite aerofc-get-version
    Most of the time is was not working hanging forever.

--- recipes-support/scripts/files/aerofc-get-version.py
+++ recipes-support/scripts/files/aerofc-get-version.py
@@ -1,29 +1,66 @@
-#! /usr/bin/env python
+#!/usr/bin/python
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
-import os,sys
+from __future__ import print_function
 from pymavlink import mavutil
+import time
+import sys
 
-if len(sys.argv)!=2:
-    print "Provide ipaddress:port like below,"
-    print "aerofc-get-version.py 127.0.0.1:5760"
-    sys.exit(0)
+tcp_ip_port = "127.0.0.1:5760"
+target_sys_id = 1
+target_comp_id = mavutil.mavlink.MAV_COMP_ID_ALL
+program_timeout = time.time() + 3
+autopilot_version = 0
 
-mav_con = mavutil.mavlink_connection('tcp:'+sys.argv[1],planner_format=False, notimestamps=True, robust_parsing=True)
+if len(sys.argv) > 1:
+	tcp_ip_port = sys.argv[1]
 
-recv_data = mav_con.recv_match(type='HEARTBEAT',blocking = True)
-
-msg = mav_con.mav.command_long_encode(0,0,520,0,1,0,0,0,0,0,0)
-mav_con.write(msg.pack(mav_con.mav))
+mav_con = mavutil.mavlink_connection('tcp:'+tcp_ip_port)
+request_msg = mav_con.mav.command_long_encode(target_sys_id, target_comp_id,
+	mavutil.mavlink.MAV_CMD_REQUEST_AUTOPILOT_CAPABILITIES,
+	0, # confirmation
+	1, # param1(request version)
+	0, # param2
+	0, # param3
+	0, # param4
+	0, # param5
+	0, # param6
+	0) # param7
+version_request_timeout = 0
 
 while True:
-    msg=mav_con.recv_msg()
-    if msg is not None:
-        if msg.get_type() is "AUTOPILOT_VERSION":
-            autopilot_version= msg.flight_sw_version
-            break
-majorVersion=(autopilot_version>>(8*3))&0xFF;
-minorVersion=(autopilot_version>>(8*2))&0xFF;
-patchVersion=(autopilot_version>>(8*1))&0xFF;
+	msg = mav_con.recv_match(blocking=True, timeout=1)
 
-fw_version=str(majorVersion)+"."+str(minorVersion)+"."+str(patchVersion)
-print "FW version: "+fw_version
+	if msg is not None and msg.get_type() is "AUTOPILOT_VERSION":
+		autopilot_version = msg.flight_sw_version
+		break
+
+	if time.time() > program_timeout:
+		break
+
+	if time.time() > version_request_timeout:
+		version_request_timeout = time.time() + 1
+		mav_con.write(request_msg.pack(mav_con.mav))
+
+if autopilot_version == 0:
+	print("AeroFC firmware version = unknown")
+	exit()
+
+majorVersion = (autopilot_version >> 24) & 0xFF
+minorVersion = (autopilot_version >> 16) & 0xFF
+patchVersion = (autopilot_version >> 8) & 0xFF
+
+fw_version = str(majorVersion) + "." + str(minorVersion) + "." + str(patchVersion)
+print("AeroFC firmware version = " + fw_version)
+
